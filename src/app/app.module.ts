7dd299bf299f89300522201e991bfb515a9e859d
meghabalaraju <kbmegha4@outlook.com>
2019-07-15 12:05:28 +0200

    pagination fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule, MatTableModule } from '@angular/material';
+import { MatToolbarModule,
+         MatButtonModule,
+         MatSidenavModule,
+         MatIconModule,
+         MatListModule,
+         MatInputModule,
+         MatPaginatorModule,
+         MatProgressSpinnerModule,
+         MatSortModule,
+         MatTableModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MensaListComponent } from './components/mensalist/mensalist.component';
 import { MensaNavComponent } from './components/mensa-nav/mensa-nav.component';
@@ -25,10 +34,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatInputModule, 
-    MatPaginatorModule, 
-    MatProgressSpinnerModule, 
-    MatSortModule, 
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
     MatTableModule,
     BrowserAnimationsModule
   ],
--- src/app/components/mensalist/mensalist.component.ts
+++ src/app/components/mensalist/mensalist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MensaService } from 'src/app/services/mensa.service';
 import { Mensa } from 'src/app/models/mensa';
 import {MatPaginator, MatTableDataSource} from '@angular/material';
+import { DataSource } from '@angular/cdk/table';
 
 @Component({
   selector: 'mensalist',
@@ -11,20 +12,21 @@ import {MatPaginator, MatTableDataSource} from '@angular/material';
 })
 export class MensaListComponent implements OnInit {
   public data: Mensa[];
-  public dataSource = new MatTableDataSource<Mensa>(this.data);
-  public displayedColumns: string[] = ['id', 'name', 'address']; 
+  public dataSource: MatTableDataSource<Mensa>;
+  public displayedColumns: string[] = ['id', 'name', 'address'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private mensaService: MensaService) {
-    mensaService.getCanteens().subscribe((response: [Mensa] )=> {
-      console.log(response);
+    mensaService.getCanteens().subscribe((response: [Mensa] ) => {
       this.data = response;
+      this.dataSource = new MatTableDataSource(this.data);
+      this.dataSource.paginator = this.paginator;
     });
    }
 
   ngOnInit() {
-    this.dataSource.paginator = this.paginator;
+
   }
 
 }
