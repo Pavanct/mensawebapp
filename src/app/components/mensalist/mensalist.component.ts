389b8a41d30de9a703c1e6740592e63ad2b8d742
meghabalaraju <kbmegha4@outlook.com>
2019-07-16 14:31:32 +0200

    filtering and sorting by city and mensa name

--- src/app/components/mensalist/mensalist.component.ts
+++ src/app/components/mensalist/mensalist.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MensaService } from 'src/app/services/mensa.service';
 import { Mensa } from 'src/app/models/mensa';
-import {MatPaginator, MatTableDataSource} from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSort} from '@angular/material';
 
 @Component({
   selector: 'mensalist',
@@ -15,13 +15,19 @@ export class MensaListComponent implements OnInit {
   public displayedColumns: string[] = ['id', 'name', 'address'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private mensaService: MensaService) {
     mensaService.getCanteens().subscribe((response: [Mensa] ) => {
       this.data = response;
       this.dataSource = new MatTableDataSource(this.data);
       this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
+   }
+
+   applyFilter(filterVlaue: string) {
+    this.dataSource.filter = filterVlaue.trim().toLowerCase();
    }
 
   ngOnInit() {
